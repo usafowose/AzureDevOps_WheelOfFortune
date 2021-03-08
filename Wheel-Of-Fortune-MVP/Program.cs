5dd679c90882667350eabe35fc3ca200ce94250f
Andrew Fowose (Aerotek US) <v-afowose@microsoft.com>
2021-03-08 18:50:53 -0500

    Original proj files without README

---
+++ Wheel-Of-Fortune-MVP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wheel_Of_Fortune_MVP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
